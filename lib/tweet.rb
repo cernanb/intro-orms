90325feba3b77250d1343acc08ff770586d18723
Cernan Bernardo <cernanb@gmail.com>
2018-06-25 22:12:51 -0700

    add orm

--- lib/tweet.rb
+++ lib/tweet.rb
@@ -1,14 +1,43 @@
+
 class Tweet
   attr_accessor :message, :username
-  ALL = []
+  attr_reader :id
 
   def self.all
-    ALL
+    sql = <<-SQL
+      SELECT * from tweets
+    SQL
+
+    tweet_hashes = DB[:conn].execute(sql)
+    tweet_hashes.map{|h| self.new(h)}
   end
 
   def initialize(props={})
+    @id = props['id']
     @message = props['message']
     @username = props['username']
-    ALL << self
+    # ALL << self
+    
+  end
+
+  def save
+
+    if persisted?
+      sql = <<-SQL
+        UPDATE tweets SET message = ?, username = ? WHERE id = ?
+      SQL
+
+      DB[:conn].execute(sql, self.message, self.username, self.id)
+    else
+      sql = <<-SQL
+        INSERT INTO tweets (message, username) VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.message, self.username)
+    end
+  end
+
+  def persisted?
+    !!id
   end
 end
--- lib/tweets_app.rb
+++ lib/tweets_app.rb
@@ -1,18 +1,22 @@
 class TweetsApp
 
   def call
-    puts 'Welcome to Twitter'
+    while true
+      puts 'Welcome to Twitter'
 
-    puts 'Enter a username:'
-    username = gets.chomp
+      puts 'Enter a username:'
+      username = gets.chomp
 
-    puts 'Enter a message:'
-    message = gets.chomp
+      puts 'Enter a message:'
+      message = gets.chomp
 
-    tweet = Tweet.new({'username' => username, 'message' => message})
+      tweet = Tweet.new({'username' => username, 'message' => message})
 
-    tweets = Tweet.all
-    render(tweets)
+      tweet.save
+
+      tweets = Tweet.all
+      render(tweets)
+    end
   end
 
   private
