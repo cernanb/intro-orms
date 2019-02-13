f38a11575ae9d9e2dd3d78dfb5aaee3fec02d272
Cernan Bernardo <github email address>
2019-02-12 16:57:34 -0800

    Update code

--- lib/tweet.rb
+++ lib/tweet.rb
@@ -1,14 +1,59 @@
 class Tweet
   attr_accessor :message, :username
+  attr_reader :id
   @@all = []
-
-  def self.all
-    @@all
-  end
-
+  
   def initialize(props={})
+    @id = props['id']
     @message = props['message']
     @username = props['username']
-    @@all << self
   end
+
+  def self.all
+    # binding.pry
+    sql = <<-DOC 
+    SELECT * FROM tweets
+    DOC
+    data = DB[:conn].execute(sql)
+    
+    data.map do |tweet_hash|
+      Tweet.new(tweet_hash)
+    end
+    # @@all
+  end
+  
+  def self.delete_by_id(id)
+    sql = <<-DOC
+    DELETE FROM tweets WHERE id = ?
+    DOC
+    
+    data = DB[:conn].execute(sql, id)
+  end
+  
+  def self.find_by_id(id)
+    sql = <<-DOC
+    SELECT * FROM tweets WHERE id = ?
+    DOC
+    
+    data = DB[:conn].execute(sql, id)
+    self.new(data[0])
+  end
+
+  def save
+    if self.id 
+      sql = <<-DOC
+        UPDATE tweets SET message = ? WHERE id = ?
+      DOC
+
+      data = DB[:conn].execute(sql, self.message, self.id)
+
+    else
+      sql = <<-DOC
+        INSERT INTO tweets (message, username) VALUES (?, ?)
+      DOC
+
+      data = DB[:conn].execute(sql, message, username)
+    end
+  end
+
 end
--- lib/tweets_app.rb
+++ lib/tweets_app.rb
@@ -4,25 +4,57 @@ class TweetsApp
     while true
       puts 'Welcome to Twitter'
 
-      puts 'Enter a username:'
-      username = gets.chomp
+      puts "What would you like to do? (t) tweet | (d) delete | (a) display all tweets | (u) update a tweet"
 
-      puts 'Enter a message:'
-      message = gets.chomp
+      input = gets.chomp
 
-      puts ""
-      tweet = Tweet.new({'username' => username, 'message' => message})
+      if input == 't'
 
-      tweets = Tweet.all
-      render(tweets)
-      puts ""
+        puts 'Enter a username:'
+        username = gets.chomp
+
+        puts 'Enter a message:'
+        message = gets.chomp
+
+        puts ""
+        tweet = Tweet.new({'username' => username, 'message' => message})
+        binding.pry
+        tweet.save
+        tweets = Tweet.all
+        render(tweets)
+        puts ""
+      elsif input == 'u'
+        tweets = Tweet.all
+        render(tweets)
+        puts "Enter the id of the tweet you want to update"
+
+        id = gets.chomp
+        tweet = Tweet.find_by_id(id)
+        puts "What would you like to change the message to?"
+        new_message = gets.chomp
+        tweet.message = new_message
+        tweet.save
+      elsif input == 'a'
+        tweets = Tweet.all
+        # binding.pry
+        render(tweets)
+
+      elsif input == 'd'
+        tweets = Tweet.all
+        render(tweets)
+        puts "Enter the id of the tweet you want to delete"
+
+        id = gets.chomp
+
+        Tweet.delete_by_id(id)
+      end
     end
   end
 
 
   def render(tweets)
-    tweets.each.with_index(1) do |tweet, i|
-      puts "#{i}. #{tweet.username} says: #{tweet.message}"
+    tweets.each do |tweet|
+      puts "#{tweet.id}. #{tweet.username} says: #{tweet.message}"
     end
   end
 end
